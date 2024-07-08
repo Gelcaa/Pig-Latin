f21db580069c27c4b3779457dfeee70e3eedf478
Gelcaa <23angelicaysabelbuan@gmail.com>
2024-07-08 22:22:50 +0800

    removed unncessary comments

--- handlers/translate.mjs
+++ handlers/translate.mjs
@@ -12,18 +12,3 @@ export const generate = async (event) => {
         }
     };
 };
-
-// Pig Latin logic
-// function pigLatin(inputTranslated) {
-//     const vowels = ["a", "e", "i", "o", "u"];
-
-//     if (vowels.includes(inputTranslated[0])) {
-//         return inputTranslated + 'yay';
-//     } else {
-//         for (let i = 0; i < inputTranslated.length; i++) {
-//             if (vowels.includes(inputTranslated[i])) {
-//                 return inputTranslated.slice(i) + inputTranslated.slice(0, i) + 'ay';
-//             }
-//         }
-//     }
-// }
